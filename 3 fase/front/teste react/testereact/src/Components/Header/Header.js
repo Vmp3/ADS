021487d25f59fdd859eb36d0de0722bb8e0aa968
Vicente Pereira <vicentepereira9486@gmail.com>
2024-03-08 21:22:00 -0300

    d

--- 3 fase/front/teste react/testereact/src/Components/Header/Header.js
+++ 3 fase/front/teste react/testereact/src/Components/Header/Header.js
@@ -4,8 +4,12 @@ import './Header.css';
 const Header = () => {
 
     function formataData(data){
-        const dia = data.getDate().toString();
-        return dia;
+        const dia = data.getDate().toString().padStart(2,'0');
+        const mes = (data.getMonth()+ 1).toString().padStart(2,'0');
+        const ano = (data.getFullYear()).toString();
+        const hora = data.getHours().toString().padStart(2,'0');
+        const minuto = data.getMinutes().toString().padStart(2,'0');
+        return `${dia}/${mes}/${ano} ${hora}:${minuto}`;
     }
     return(
         <div className='head-container'> 
