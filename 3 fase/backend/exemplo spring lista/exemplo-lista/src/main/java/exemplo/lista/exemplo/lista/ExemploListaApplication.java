42ab04fe03a4b3b6673c0e6e4ae612dab654d49f
Vicente Pereira <vicentepereira9486@gmail.com>
2024-03-11 19:38:12 -0300

    dddd

---
+++ 3 fase/backend/exemplo spring lista/exemplo-lista/src/main/java/exemplo/lista/exemplo/lista/ExemploListaApplication.java
@@ -0,0 +1,13 @@
+package exemplo.lista.exemplo.lista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploListaApplication.class, args);
+	}
+
+}
---
+++ 3 fase/backend/exemplo spring lista/exemplo-lista/src/test/java/exemplo/lista/exemplo/lista/ExemploListaApplicationTests.java
@@ -0,0 +1,13 @@
+package exemplo.lista.exemplo.lista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
