636ad49e4f23d65587e079db51441c3c8a07e533
Vicente Pereira <vicentepereira9486@gmail.com>
2024-03-18 19:57:53 -0300

    hy

--- 3 fase/2 backend/carros/src/main/java/com/carros/repository/CarroRepository.java
+++ 3 fase/2 backend/carros/src/main/java/com/carros/repository/CarroRepository.java
@@ -1,8 +1,10 @@
 package com.carros.repository;
+import java.util.List;
+
 import com.carros.model.Carro;
 public interface CarroRepository {
     void save(Carro carros);
     List<Carro> findAll();
-    void update (Long id, Carro carro,);
+    void update (Long id, Carro carro);
     void delete (Long id);
 }
