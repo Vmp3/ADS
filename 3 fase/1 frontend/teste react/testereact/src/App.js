53a11ec5c39bfed34ab3bccfa1d37ccc0c6cefbf
Vicente Pereira <vicentepereira9486@gmail.com>
2024-04-05 19:34:25 -0300

    dqwdw

--- 3 fase/1 frontend/teste react/testereact/src/App.js
+++ 3 fase/1 frontend/teste react/testereact/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Components/Header/Header';
+import Section from './Components/Section/Section';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Section>Section 01 </Section>
+      <Section>Section 01 </Section>
     </div>
   );
 }
